25d52cf6e03db0014feb02b15366b1c24ab5e17e
Marc Auf der Heyde <2326744a@student.gla.ac.uk>
2021-11-21 12:49:48 +0000

    Pushing tesco scrape work in endpoints. Works for all products I've tested which is about 20 random products

--- app/api/endpoints.py
+++ app/api/endpoints.py
@@ -1,5 +1,7 @@
+import requests
 from datetime import datetime
 from flask import request
+from bs4 import BeautifulSoup
 
 # http://flask-restplus.readthedocs.io
 from flask_restx import Resource
@@ -12,7 +14,31 @@ class TescoProduct(Resource):
 
     def get(self, product_id):
         timestamp = datetime.utcnow().isoformat()
-        return {'timestamp': timestamp, 'product': product_id}
+        
+        # Headers so we don't get blocked by tesco
+        headers={
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
+        }
+        
+        # Get the page with reuest and convert to beautiful soup
+        url = "https://www.tesco.com/groceries/en-GB/products/" + str(product_id)
+        page = requests.get(url, headers=headers, timeout=120)
+        soup = BeautifulSoup(page.content, 'html.parser')
+
+        # Find the description of the product
+        descriptors = soup.find_all(class_="product-info-block__content-item")
+        
+        # Find the product name
+        product_name = soup.find("title").get_text().split(" - ")[0]
+        
+        # Create product description from descriptors
+        product_description = " ".join(d.get_text() for d in descriptors)
+        
+        # Find the product price
+        product_price = soup.find(class_="price-per-sellable-unit").get_text()
+        
+        return {'timestamp': timestamp, 'product': product_id, 'product_name': product_name, 
+                'product_description': product_description, 'product_price': product_price}
 
     def post(self, product_id):
         json_payload = request.json
