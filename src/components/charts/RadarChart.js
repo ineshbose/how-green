3405dd710740cbe91239885c6ba1c594a2b582fd
Hector Jones <2375347j@student.gla.ac.uk>
2021-11-25 17:55:21 +0000

    added two more graphs with formatting. also added API endpoint to GET product data from backend. TODO: fix CORS related error

---
+++ src/components/charts/RadarChart.js
@@ -0,0 +1,19 @@
+import { Radar, mixins } from 'vue-chartjs'
+const { reactiveProp } = mixins
+
+export default {
+  extends: Radar,
+  mixins: [reactiveProp],
+  props: {
+    labels: Array,
+    datasets: Object
+  },
+  mounted () {
+    // this.chartData is created in the mixin.
+    // If you want to pass options please create a local options object
+    this.renderChart({
+      labels: this.labels,
+      datasets: this.datasets,
+    }, {responsive: true, maintainAspectRatio: false})
+  }
+}
