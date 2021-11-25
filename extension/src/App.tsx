011a48927322b6d252adf2ebdc69875263462be8
Marc Auf der Heyde <2326744a@student.gla.ac.uk>
2021-11-25 21:52:04 +0000

    Implemented most of the functionality of the extension. Front end is still bad but the functionality is coming along nicely!

--- extension/src/App.tsx
+++ extension/src/App.tsx
@@ -3,23 +3,33 @@ import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import Tabs from "react-bootstrap/Tabs";
 import Tab from "react-bootstrap/Tab";
+import ListGroup from "react-bootstrap/ListGroup";
 import logo from "./logo.svg";
 import "./App.css";
 
 export default class App extends React.Component<{}, any> {
+  // Oh YeS a GlObAl VaRiAbLe We LoVe ThEm
   static productID: string;
 
+  // Our constructor for our Reac component
   constructor(props: any) {
     super(props);
     this.state = {tabs: [], curTescoProductID: "NoProduct"};
     App.productID = "NoProduct";
   }
 
+  /**
+   * Here go all the things we want to happen when the component is
+   * first mounted. Specifically, this refers to setting a listener 
+   * to get the URL from the active tab every 4 seconds (this is arbitrary).
+   */
   componentDidMount() {
     this.getTabs();
     this.getTabURL();
+    setInterval(this.getTabURL, 500); // runs every 0.5 seconds.
   }
 
+  // Function which gets all chrome tabs open - currently unused
   getTabs() {
     chrome.tabs.query(
       {active: true, lastFocusedWindow: true},
@@ -27,18 +37,31 @@ export default class App extends React.Component<{}, any> {
     );
   }
 
+  // Function which updates the current tesco product ID in App state
   updateTescoProductID(id: string){
     this.setState({ curTescoProductID: id });
   }
 
+  // Function which retrieves the current tesco product ID from App state
   reportTescoProductID = () => {
-    if(this.state.curTescoProductID != "NoProduct"){
-      alert(this.state.curTescoProductID);
+    // We need to make sure that the product ID has been set and isn't undefined
+    if(typeof this.state.curTescoProductID === "undefined"){
+      alert("Couldn't find a product ID, please try again!");
+    }
+    else{
+      return(this.state.curTescoProductID);
     }
   }
 
+  /**
+   * Function which queries the available chrome tabs and returns the one
+   * that's currently active. It then checks whether the active tab is a 
+   * tesco.com page, if yes, it extracts the product ID from the URL and saves
+   * this to the application state with the help of a global variable.
+   * Is probably doable otherwise, but I think we need to update the manifest and
+   * a bunch of other stuff which could be a nightmare!
+   */
   getTabURL = () => {
-    //this.reportTescoProductID();
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 
       // since only one tab should be active and in the current window at once
@@ -46,40 +69,84 @@ export default class App extends React.Component<{}, any> {
       var activeTab = tabs[0];
       if(activeTab.url != null){
         const urlComponents = activeTab.url.split("/");
+
+        // Save to global variable from async so we can capture this to state
         if (urlComponents.includes("www.tesco.com")){
-          //alert(App.productID);
-          //alert(urlComponents.at(-1));
           App.productID = urlComponents.at(-1)!;
         }
       }
    });
+
+   // Update our App state
    this.updateTescoProductID(App.productID);
-   this.reportTescoProductID();
   }
 
+  /**
+   * Function responsible for handling the "what's in the score" button.
+   * Finds the current Tesco Product ID and creates a URL to visit in a new tab.
+   */
+  raiseScoreClicked = () => {
+    //alert("Score clicked");
 
-  raiseScoreClicked(){
-    chrome.tabs.create({
-      url: 'http://localhost:8080/visualisation'
-    });
+    // We need to make sure that the product ID has been set and isn't undefined
+    if(typeof this.reportTescoProductID() === "undefined"){
+      alert("Couldn't find a product ID, please try again!");
+    }
+
+    else{
+
+      // Make sure the found ID is valid, i.e., not NoProduct (the starting default)
+      if(this.reportTescoProductID() != "NoProduct"){
+        //alert("We have a product ID");
+        //alert(this.reportTescoProductID());
+
+        // Brew up a nice URL to visit and open it in a new tab
+        let scoreUrl = 'http://localhost:8080/product/tesco/' + this.reportTescoProductID();
+        chrome.tabs.create({
+          url: scoreUrl
+        });
+      }
+      else{
+        alert("Couldn't find a product ID, please try again!");
+      }
+    }
   }
 
+  /**
+   * Function responsible for handling the "product alternatives" button.
+   * Finds the current Tesco Product ID and creates a URL to visit in a new tab
+   * shwoing the alternatives for that product.
+   */
   raiseAlternativesClicked(){
     chrome.tabs.create({
       url: 'http://localhost:8080/visualisation'
     });
   }
 
+  /**
+   * Function responsible for handling the alternatives presented on the extension.
+   * Should do a search on Tesco for a key word from the product (how do we get that?)
+   * Aka, how do I get 'pizza' from 'Tesco Finest Ham Mushroom Mascarpone Pizza 450G'??
+   * Once we've done the search, should get back a search result list where we can just
+   * take the links and pop them into the view.
+   * 
+   * Right now this just takes you to Garlic & Parsley -- hardcoded af.
+   */
+     raiseAlternativeClicked(){
+      chrome.tabs.create({
+        url: 'https://www.tesco.com/groceries/en-GB/products/296481201'
+      });
+    }
+
+  // Standard render method to return our React Component
   render() {
+    let id = this.reportTescoProductID();
     return (
       <Card text="white" style={{ width: '20rem', backgroundColor:'#0DD98F', textAlign: 'center'}}>
         <Card.Img src={logo} />
         <Tabs defaultActiveKey="score" id="uncontrolled-tab-example" className="mb-3">
           <Tab eventKey="score" title="Score">
             <Card.Body>
-              {/* Really bad way to do positioning lol
-                  For anyone reading this, I know there's
-                  probably a better way, but it became a meme*/}
               <Card.Text>
                 This Tab will display the score calculated for the user's product page.
               </Card.Text>
@@ -87,6 +154,14 @@ export default class App extends React.Component<{}, any> {
               <Card.Text style={{ fontSize: 40, width: '18rem', color:'green',  backgroundColor:'#FFFFFF', textAlign: 'center'}}>
                 76%
               </Card.Text>
+              <div>
+                <h1>Hello!</h1>
+                {typeof id === "string" &&
+                  <h2>
+                    You are visiting {id} tesco product.
+                  </h2>
+                }
+              </div>
               <Card.Text>{"\n"}</Card.Text>
               <Button onClick={this.raiseAlternativesClicked} variant="primary">Alternatives</Button>
                 &nbsp;&nbsp;&nbsp;
@@ -95,17 +170,17 @@ export default class App extends React.Component<{}, any> {
           </Tab>
           <Tab eventKey="alternatives" title="Alternatives">
             <Card.Body>
-                {/* Really bad way to do positioning lol
-                    For anyone reading this, I know there's
-                    probably a better way, but it became a meme*/}
                 <Card.Text>
-                  This page will show product alternatives for the current product you are viewing. This page
-                  will have some placeholder imagery for the case when a user is not on a product page.
+                  Some quick alternative products you can choose!
                 </Card.Text>
                 <Card.Text>{"\n"}</Card.Text>
-                <Button onClick={this.getTabURL} variant="primary">Tab URL?</Button>
-                &nbsp;&nbsp;&nbsp;
-                <Button onClick={this.getTabURL} variant="primary">Tab URL?</Button>
+                <ListGroup>
+                  <ListGroup.Item action onClick={this.raiseAlternativeClicked}>Cras justo odio  - 78%</ListGroup.Item>
+                  <ListGroup.Item action onClick={this.raiseAlternativeClicked}>Dapibus ac facil - 72%</ListGroup.Item>
+                  <ListGroup.Item action onClick={this.raiseAlternativeClicked}>Morbi leo risus  - 69%</ListGroup.Item>
+                  <ListGroup.Item action onClick={this.raiseAlternativeClicked}>Porta ac consecti- 64%</ListGroup.Item>
+                  <ListGroup.Item action onClick={this.raiseAlternativeClicked}>Vestibulum at ero- 57%</ListGroup.Item>
+                </ListGroup>
               </Card.Body>
           </Tab>
         </Tabs>
