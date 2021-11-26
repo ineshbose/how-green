8733d89e5b424445b4920ff1dfa9b2107193260d
Inesh Bose <2504266b@student.gla.ac.uk>
2021-11-26 13:35:32 +0000

    Refactored

--- extension/src/App.tsx
+++ extension/src/App.tsx
@@ -1,4 +1,6 @@
 import * as React from "react";
+import Spinner from "react-bootstrap/Spinner";
+import Badge from "react-bootstrap/Badge";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import Tabs from "react-bootstrap/Tabs";
@@ -7,183 +9,102 @@ import ListGroup from "react-bootstrap/ListGroup";
 import logo from "./logo.svg";
 import "./App.css";
 
-export default class App extends React.Component<{}, any> {
-  // Oh YeS a GlObAl VaRiAbLe We LoVe ThEm
-  static productID: string;
+interface Alternative {
+  id: string | number;
+  name: string;
+  score: string | number;
+};
 
-  // Our constructor for our Reac component
+export default class App extends React.Component<{}, any> {
+
   constructor(props: any) {
     super(props);
-    this.state = {tabs: [], curTescoProductID: "NoProduct"};
-    App.productID = "NoProduct";
+    this.state = {tabs: [], productID: "", alternatives: []};
   }
 
-  /**
-   * Here go all the things we want to happen when the component is
-   * first mounted. Specifically, this refers to setting a listener 
-   * to get the URL from the active tab every 4 seconds (this is arbitrary).
-   */
-  componentDidMount() {
-    this.getTabs();
-    this.getTabURL();
+  async componentDidMount() {
+    await this.getTabURL();
+    this.getAlternatives();
     setInterval(this.getTabURL, 500); // runs every 0.5 seconds.
   }
 
-  // Function which gets all chrome tabs open - currently unused
-  getTabs() {
-    chrome.tabs.query(
-      {active: true, lastFocusedWindow: true},
-      tabs => this.setState({ tabs: tabs })
-    );
-  }
-
-  // Function which updates the current tesco product ID in App state
-  updateTescoProductID(id: string){
-    this.setState({ curTescoProductID: id });
-  }
-
-  // Function which retrieves the current tesco product ID from App state
   reportTescoProductID = () => {
-    // We need to make sure that the product ID has been set and isn't undefined
-    if(typeof this.state.curTescoProductID === "undefined"){
-      alert("Couldn't find a product ID, please try again!");
-    }
-    else{
-      return(this.state.curTescoProductID);
-    }
+    return this.state.productID || alert("Couldn't find a product ID, please try again!");
   }
 
-  /**
-   * Function which queries the available chrome tabs and returns the one
-   * that's currently active. It then checks whether the active tab is a 
-   * tesco.com page, if yes, it extracts the product ID from the URL and saves
-   * this to the application state with the help of a global variable.
-   * Is probably doable otherwise, but I think we need to update the manifest and
-   * a bunch of other stuff which could be a nightmare!
-   */
-  getTabURL = () => {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-
-      // since only one tab should be active and in the current window at once
-      // the return variable should only have one entry
+  async getTabURL() {
+    await chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
       var activeTab = tabs[0];
-      if(activeTab.url != null){
-        const urlComponents = activeTab.url.split("/");
-
-        // Save to global variable from async so we can capture this to state
-        if (urlComponents.includes("www.tesco.com")){
-          App.productID = urlComponents.at(-1)!;
-        }
+      if (activeTab.url && activeTab.url.includes("tesco.com")) {
+        this.setState({ productID: activeTab.url.split("?")[0].split("/").pop() });
       }
    });
-
-   // Update our App state
-   this.updateTescoProductID(App.productID);
   }
 
-  /**
-   * Function responsible for handling the "what's in the score" button.
-   * Finds the current Tesco Product ID and creates a URL to visit in a new tab.
-   */
-  raiseScoreClicked = () => {
-    //alert("Score clicked");
-
-    // We need to make sure that the product ID has been set and isn't undefined
-    if(typeof this.reportTescoProductID() === "undefined"){
-      alert("Couldn't find a product ID, please try again!");
-    }
-
-    else{
-
-      // Make sure the found ID is valid, i.e., not NoProduct (the starting default)
-      if(this.reportTescoProductID() != "NoProduct"){
-        //alert("We have a product ID");
-        //alert(this.reportTescoProductID());
-
-        // Brew up a nice URL to visit and open it in a new tab
-        let scoreUrl = 'http://localhost:8080/product/tesco/' + this.reportTescoProductID();
-        chrome.tabs.create({
-          url: scoreUrl
-        });
-      }
-      else{
-        alert("Couldn't find a product ID, please try again!");
-      }
-    }
+  getAlternatives() {
+    this.setState({
+      alternatives: [
+        {
+          "id": "296481201",
+          "name": "Cras justo odio",
+          "score": 78,
+        },
+        {
+          "id": "296481201",
+          "name": "Cras justo odio",
+          "score": 78,
+        },
+        {
+          "id": "296481201",
+          "name": "Cras justo odio",
+          "score": 78,
+        },
+      ]
+    })
   }
 
-  /**
-   * Function responsible for handling the "product alternatives" button.
-   * Finds the current Tesco Product ID and creates a URL to visit in a new tab
-   * shwoing the alternatives for that product.
-   */
-  raiseAlternativesClicked(){
-    chrome.tabs.create({
-      url: 'http://localhost:8080/visualisation'
-    });
-  }
-
-  /**
-   * Function responsible for handling the alternatives presented on the extension.
-   * Should do a search on Tesco for a key word from the product (how do we get that?)
-   * Aka, how do I get 'pizza' from 'Tesco Finest Ham Mushroom Mascarpone Pizza 450G'??
-   * Once we've done the search, should get back a search result list where we can just
-   * take the links and pop them into the view.
-   * 
-   * Right now this just takes you to Garlic & Parsley -- hardcoded af.
-   */
-     raiseAlternativeClicked(){
-      chrome.tabs.create({
-        url: 'https://www.tesco.com/groceries/en-GB/products/296481201'
-      });
-    }
-
-  // Standard render method to return our React Component
   render() {
-    let id = this.reportTescoProductID();
+    const { productID, alternatives } = this.state;
     return (
       <Card text="white" style={{ width: '20rem', backgroundColor:'#0DD98F', textAlign: 'center'}}>
-        <Card.Img src={logo} />
-        <Tabs defaultActiveKey="score" id="uncontrolled-tab-example" className="mb-3">
-          <Tab eventKey="score" title="Score">
-            <Card.Body>
-              <Card.Text>
-                This Tab will display the score calculated for the user's product page.
-              </Card.Text>
-              <Card.Text>{"\n"}</Card.Text>
-              <Card.Text style={{ fontSize: 40, width: '18rem', color:'green',  backgroundColor:'#FFFFFF', textAlign: 'center'}}>
-                76%
-              </Card.Text>
-              <div>
-                <h1>Hello!</h1>
-                {typeof id === "string" &&
+        {
+          (!productID
+          ? (
+          <Spinner animation="border" role="status" className="m-4">
+            <span className="visually-hidden">Loading...</span>
+          </Spinner>
+            )
+          : (
+          <>
+            <Card.Img src={logo} />
+            <Tabs defaultActiveKey="score" className="mb-3 nav-fill">
+              <Tab eventKey="score" title="Score">
+                <Card.Body>
                   <h2>
-                    You are visiting {id} tesco product.
+                    {productID} <Badge bg="light" text="dark">76%</Badge>
                   </h2>
-                }
-              </div>
-              <Card.Text>{"\n"}</Card.Text>
-              <Button onClick={this.raiseAlternativesClicked} variant="primary">Alternatives</Button>
-                &nbsp;&nbsp;&nbsp;
-                <Button onClick={this.raiseScoreClicked} variant="primary">In the Score?</Button>
-            </Card.Body>
-          </Tab>
-          <Tab eventKey="alternatives" title="Alternatives">
-            <Card.Body>
-                <Card.Text>
-                  Some quick alternative products you can choose!
-                </Card.Text>
-                <Card.Text>{"\n"}</Card.Text>
-                <ListGroup>
-                  <ListGroup.Item action onClick={this.raiseAlternativeClicked}>Cras justo odio  - 78%</ListGroup.Item>
-                  <ListGroup.Item action onClick={this.raiseAlternativeClicked}>Dapibus ac facil - 72%</ListGroup.Item>
-                  <ListGroup.Item action onClick={this.raiseAlternativeClicked}>Morbi leo risus  - 69%</ListGroup.Item>
-                  <ListGroup.Item action onClick={this.raiseAlternativeClicked}>Porta ac consecti- 64%</ListGroup.Item>
-                  <ListGroup.Item action onClick={this.raiseAlternativeClicked}>Vestibulum at ero- 57%</ListGroup.Item>
-                </ListGroup>
-              </Card.Body>
-          </Tab>
-        </Tabs>
+                  <Button onClick={() => chrome.tabs.create({ url: `http://localhost:8080/product/tesco/${productID}/visualisation`})} variant="primary">Alternatives</Button>
+                  &nbsp;&nbsp;&nbsp;
+                  <Button onClick={() => chrome.tabs.create({ url: `http://localhost:8080/product/tesco/${productID}` })} variant="primary">In the Score?</Button>
+                </Card.Body>
+              </Tab>
+              <Tab eventKey="alternatives" title="Alternatives">
+                <Card.Body>
+                  <ListGroup>
+                    {
+                      alternatives.map((alternative: Alternative) => (
+                        <ListGroup.Item action onClick={() => chrome.tabs.create({ url: `https://www.tesco.com/groceries/en-GB/products/${alternative.id}`})}>
+                          {alternative.name} <Badge bg="info">{alternative.score}%</Badge>
+                        </ListGroup.Item>
+                      ))
+                    }
+                  </ListGroup>
+                </Card.Body>
+              </Tab>
+            </Tabs>
+        </>
+          )
+        )}
       </Card>
     );
   }
